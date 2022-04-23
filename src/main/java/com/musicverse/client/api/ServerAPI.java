25cd0352437e601f99ed5db17e77a9fccd298085
Róbert Poór <10374444+rpoor@users.noreply.github.com>
2022-04-23 12:56:38 +0200

    audio download, upload, and playback.

--- src/main/java/com/musicverse/client/api/ServerAPI.java
+++ src/main/java/com/musicverse/client/api/ServerAPI.java
@@ -2,12 +2,14 @@ package com.musicverse.client.api;
 
 import com.falsepattern.json.node.JsonNode;
 import com.falsepattern.json.node.ObjectNode;
-import lombok.SneakyThrows;
+import com.musicverse.client.IOUtil;
 import lombok.val;
 
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class ServerAPI {
 	private static ServerAPI instance;
@@ -127,7 +129,42 @@ public class ServerAPI {
 		});
 	}
 
+	//Downloads a song from the server, and stores it in the local file storage.
+	public boolean downloadSongData(int songID) {
+		val payload = new ObjectNode();
+		payload.set("song_id", songID);
+		try {
+			return queryServerRaw("downloadSongData", Method.POST, payload, (code, response) -> {
+				if (code != 200) return false;
+				val file = IOUtil.getSongFile(songID);
+				try {
+					IOUtil.streamToFile(response, file);
+					return true;
+				} catch (IOException e) {
+					e.printStackTrace();
+					return false;
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
 
+	//Uploads a song file to the server.
+	public boolean uploadSongData(File songFile, int songID) throws IOException {
+		val fileSize = Files.size(songFile.toPath());
+		if (fileSize > 0xFFFFFFFFL) throw new IllegalArgumentException("File " + songFile + " is too big to upload!");
+
+		val prefixStream = new ByteArrayOutputStream();
+		IOUtil.writeInt(songID, prefixStream);
+		IOUtil.writeInt((int)fileSize, prefixStream);
+		val prefix = prefixStream.toByteArray();
+		val prefixInput = new ByteArrayInputStream(prefix);
+		try (val fileInput = new BufferedInputStream(new FileInputStream(songFile)); val sequence = new SequenceInputStream(prefixInput, fileInput)) {
+			return queryServerJson("uploadSongData", Method.POST, fileSize + prefix.length, sequence, (code, response) -> response.getString("status").equals("ok"));
+		}
+	}
 
 	public JsonNode songsByPlaylist(int playlist_id){
 		if(playlist_id < 0)
@@ -211,17 +248,68 @@ public class ServerAPI {
 		return queryServerJson("create_artist", Method.POST, payload, (code, response) -> response.getString("status").equals("ok"));
 	}
 
-
+	//Internal communication code below
 
 	private <R> R queryServerJson(String url, Method method, JsonNode request, ServerQueryCallback<JsonNode, R> callback) {
-		return queryServerBinary(url, method, request, (code, bin) -> {
-			val json = JsonNode.parse(new String(bin));
-			return callback.processResponse(code, json);
+		val raw = request.toString().getBytes(StandardCharsets.UTF_8);
+		val in = new ByteArrayInputStream(raw);
+		return queryServerJson(url, method, raw.length, in, callback);
+	}
+
+	private <R> R queryServerJson(String url, Method method, long payloadSize, InputStream payload, ServerQueryCallback<JsonNode, R> callback) {
+		try {
+			return queryServerBinary(url, method, payloadSize, payload, (code, bin) -> {
+				JsonNode json;
+				if (bin.length == 0) {
+					json = new ObjectNode();
+					json.set("status", "client_error-no_data");
+					json.set("details", "The server did not send any JSON data in the response.");
+				} else {
+					try {
+						json = JsonNode.parse(new String(bin));
+					} catch (Exception e) {
+						e.printStackTrace();
+						json = new ObjectNode();
+						json.set("status", "client_error-corrupted_data");
+						json.set("details", "The server sent data that could not be parsed as JSON.");
+					}
+				}
+				return callback.processResponse(code, json);
+			});
+		} catch (IOException e) {
+			val fakeResponse = new ObjectNode();
+			fakeResponse.set("status", "client_error-query_failed");
+			fakeResponse.set("details", "Error while querying server. This response was created by the client as a fallback.");
+			return callback.processResponse(0, fakeResponse);
+		}
+	}
+
+	private <R> R queryServerBinary(String url, Method method, JsonNode request, ServerQueryCallback<byte[], R> callback) throws IOException {
+		val raw = request.toString().getBytes(StandardCharsets.UTF_8);
+		val in = new ByteArrayInputStream(raw);
+		return queryServerBinary(url, method, raw.length, in, callback);
+	}
+
+	private <R> R queryServerBinary(String url, Method method, long payloadSize, InputStream payload, ServerQueryCallback<byte[], R> callback) throws IOException {
+		return queryServerRaw(url, method, payloadSize, payload, (code, is) -> {
+			var data = new byte[0];
+			if (is != null)
+				try {
+					data = is.readAllBytes();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return callback.processResponse(code, data);
 		});
 	}
 
-	@SneakyThrows
-	private <R> R queryServerBinary(String url, Method method, JsonNode request, ServerQueryCallback<byte[], R> callback) {
+	private <R> R queryServerRaw(String url, Method method, JsonNode request, ServerQueryCallback<InputStream, R> callback) throws IOException {
+		val raw = request.toString().getBytes(StandardCharsets.UTF_8);
+		val in = new ByteArrayInputStream(raw);
+		return queryServerRaw(url, method, raw.length, in, callback);
+	}
+
+	private <R> R queryServerRaw(String url, Method method, long payloadSize, InputStream payload, ServerQueryCallback<InputStream, R> callback) throws IOException {
 		val requestURI = String.format("%s:%d/%s", hostname, port, url);
 		val conn = (HttpURLConnection) new URL(requestURI).openConnection();
 		conn.setRequestProperty("User-Agent", "musicverse-client");
@@ -230,15 +318,18 @@ public class ServerAPI {
 		conn.setRequestMethod(method.name());
 		conn.connect();
 		val os = conn.getOutputStream();
-		os.write(request.toString().getBytes(StandardCharsets.UTF_8));
+		IOUtil.pipe(payload, os, payloadSize);
 		os.flush();
 		os.close();
 		val responseCode = conn.getResponseCode();
-		if (responseCode != 200) {
-			throw new IllegalStateException("ERROR: Server returned error code " + responseCode + ": " + conn.getResponseMessage());
+		InputStream is;
+		try {
+			is = conn.getInputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+			is = null;
 		}
-		val content = conn.getInputStream().readAllBytes();
-		return callback.processResponse(conn.getResponseCode(), content);
+		return callback.processResponse(responseCode, is);
 	}
 
 	public interface ServerQueryCallback<T, R> {
--- src/main/java/com/musicverse/client/player/MediaManager.java
+++ src/main/java/com/musicverse/client/player/MediaManager.java
@@ -1,61 +1,40 @@
 package com.musicverse.client.player;
 
+import com.musicverse.client.IOUtil;
+import com.musicverse.client.api.ServerAPI;
 import lombok.Getter;
 import lombok.val;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.*;
 
 public class MediaManager {
+    public static final MediaManager instance = new MediaManager();
+
     @Getter
     private final AudioPlayer player = new AudioPlayer();
-    private final Map<String, File> loadedSongs = new HashMap<>();
-    private String currentSong;
+    private final Map<Integer, File> loadedSongs = new HashMap<>();
 
-    public Optional<String> currentlyPlayingSong() {
-        return Optional.ofNullable(currentSong);
-    }
-
-    public void setCurrentSong(String ID) throws MusicPlayerException {
-        if (!loadedSongs.containsKey(ID)) throw new IllegalStateException("Song with ID \"" + ID + "\" is not loaded!");
+    public void setCurrentSong(int ID) throws MusicPlayerException {
+        if (!loadedSongs.containsKey(ID)) {
+            loadSong(ID);
+        }
         val song = loadedSongs.get(ID);
         player.loadSong(song);
     }
 
-    public void loadSong(String ID, File file) {
-        if (!file.exists()) throw new IllegalStateException("Cannot load a song from a file that does not exists!");
-        if (loadedSongs.containsKey(ID)) throw new IllegalStateException("Cannot load a song with the same ID twice!");
-        loadedSongs.put(ID, file);
+    private void loadSong(int ID) throws MusicPlayerException {
+        val songFile = IOUtil.getSongFile(ID);
+        if (!songFile.exists()) {
+            downloadSong(ID);
+        }
+        loadedSongs.put(ID, songFile);
     }
 
-    public void downloadSong(String ID, File file, String url, String requestMethod, Map<String, String> requestProperties) throws IOException {
-        if (loadedSongs.containsKey(ID)) throw new IllegalStateException("Cannot download a song with the same ID twice!");
-        if (file.exists()) throw new IllegalStateException("Cannot download a song to a file that already exists!");
-        val connection = (HttpURLConnection) new URL(url).openConnection();
-        try {
-            requestProperties.forEach(connection::setRequestProperty);
-            connection.setRequestMethod(requestMethod);
-            val response = connection.getResponseCode();
-            if (!(response == 200)) {
-                connection.disconnect();
-                throw new IllegalStateException("Failed to download song: Response code " + response);
-            }
-            val in = connection.getInputStream();
-            file.mkdirs();
-            val out = new FileOutputStream(file);
-            val buf = new byte[1024];
-            var amount = 0;
-            while((amount = in.read(buf)) > 0) {
-                out.write(buf, 0, amount);
-            }
-            out.close();
-        } finally {
-            connection.disconnect();
+    private void downloadSong(int ID) throws MusicPlayerException {
+        if (!ServerAPI.getInstance().downloadSongData(ID)) {
+            throw new MusicPlayerException("Could not download song with ID " + ID + " from server!");
         }
-        loadSong(ID, file);
     }
 }
