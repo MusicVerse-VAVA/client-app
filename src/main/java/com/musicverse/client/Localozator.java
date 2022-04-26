b8045a06d35e85891ca0280cad6f7b25f609f533
Róbert Poór <10374444+rpoor@users.noreply.github.com>
2022-04-26 22:46:58 +0200

    final fixes

--- src/main/java/com/musicverse/client/Localozator.java
+++ src/main/java/com/musicverse/client/Localozator.java
@@ -10,25 +10,12 @@ import java.util.ResourceBundle;
 public class Localozator {
     private static Locale locale = Locale.getDefault();
 
-    private static final File file = new File("src/main/resources/bundle");
-
-    private static URL[] urls = new URL[0];
-
-    static {
-        try {
-            urls = new URL[]{file.toURI().toURL()};
-        } catch (MalformedURLException malformedURLException) {
-            malformedURLException.printStackTrace();
-        }
-    }
-
     public static void setLocale(Locale l){
         locale = l;
     }
 
     public static ResourceBundle getResourceBundle(){
-        ClassLoader loader = new URLClassLoader(urls);
-        return ResourceBundle.getBundle("Language", locale, loader);
+        return ResourceBundle.getBundle("bundle.Language", locale);
     }
 
 }
